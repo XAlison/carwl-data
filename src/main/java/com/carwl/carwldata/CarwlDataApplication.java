43261e596722010e48c9d22961ed3c92a5a72289
Administrator <413527735@qq.com>
2020-01-03 15:52:35 +0800

    Initial commit

---
+++ src/main/java/com/carwl/carwldata/CarwlDataApplication.java
@@ -0,0 +1,13 @@
+package com.carwl.carwldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwlDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarwlDataApplication.class, args);
+    }
+
+}
